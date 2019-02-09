9fabdc9951fce549600101c82335cc9f31bd8e88
Quinntin Gieseke <qgieseke@gmail.com>
2019-02-08 20:04:34 -0800

    fixed bugs

--- assn1.1.1/README.txt
+++ assn1.1.1/README.txt
@@ -5,9 +5,5 @@ I additionally copied no code from the above sites, only looked for refrence, re
 
 Known bugs:
 
-Ctrl+c will not start a newline, only print out a heart (might be my windows terminal).
-
-Sometimes typing exit will not cause the loop to finish (return 0 does not return?), couldnt debug this as the print statement directly before the return always triggers.
-
 Cannot compile with -Werror, since I got an error saying wait() has not been defined, despite including its .h, and functionality being present. However, this is the only error I compile with so hopefully it is OK.
 
--- assn1.1.1/kapish.c
+++ assn1.1.1/kapish.c
@@ -15,88 +15,92 @@ void handle_sigint(int sig);
 int cid;
 
 int main(int argc, const char*argv[]){
-//	for(int i = 0; i < argc; i++){
-//		printf("%s\n", argv[i]);
-//	}
-//	return 0;
-
 	
-	init();
 	signal(SIGINT, handle_sigint);
-	char *args[512] = {""};
-	exec("pwd", args);
-	exec("ls", args);
+	init();
 	loop();
 	return(0);
 }
 
+//Reads the .kapishrc file and execs all commands within, echoing what is done
 int init(){
 	char rc_dir[128] = {0};
 	char cmd[512] = {0};
 	
-	//printf("%s\n", getenv("HOME"));
- 	
+	//get .kapishrc filepath 	
 	strncat(rc_dir, getenv("HOME"), 128);
-	//printf("%s\n", rc_dir);
-	
 	strcat(rc_dir, "/.kapishrc");
-	//printf("%s\n", rc_dir);
+	
 	FILE *rc = fopen(rc_dir, "r");
-	
-	printf(".Kapishrc opened\n");
-	
-	
-	while(fgets(cmd, 512, rc) != NULL){
-		
-		printf("%s", cmd);		//replace with exec later
+	if(rc == NULL){
+		printf("No .kapishrc file found\n");
+	} else {
+		printf(".Kapishrc opened\n");
+		char * args[512] = {""};
+			
+		while(fgets(cmd, 512, rc) != NULL){
+			memset(args, 0, 512);
+			printf("%s", cmd);
+			if(strcmp(cmd, "\n") == 0) continue;
+			parse_cmd(cmd, args);
+			if(exec(cmd, args)){
+				printf("Invalid cmd\n");
+				exit(0);
+			}
+			free_args(args);
+		}
 	}
 	return(0);
 }
 
+//Main program loop, will parse commands given and exec them if valid. Has 4 builtin commands: set/unset env, cd, and exit
 int loop(){
 	char cmd[512] = {0};
 	char *args[512] = {""};
 	printf("? ");
 
-	while (fgets(cmd, 512, stdin) != NULL){
-		
+	while (fgets(cmd, 512, stdin) != NULL){		
 		
 		memset(args, 0, 512);	
+		//override for empty lines to avoid passing null values to other functions
 		if (strcmp(cmd, "\n") == 0){
 			printf("? ");
 			continue;
 		}
-		//printf("cmd is |%s|\n", cmd);
+		
 		parse_cmd(cmd, args);
-		//printf("cmd is |%s|\n", cmd);
+		
 		if(strcmp(cmd, "setenv") == 0){
 			//printf("setenv\n");
 			char *val = "";
 			if(args[2] != 0) val = args[2];
 			setenv(args[1], val, 1);
-			printf("? ");
-			continue;
+
 		} else if (strcmp(cmd, "unsetenv") ==0){
 			//printf("unsetenv\n");
 			unsetenv(args[1]);
-			printf("? ");
-			continue;
+
 		}else if (strcmp(cmd, "cd") == 0){
 			//printf("cd\n");
 			char * dir = getenv("HOME");
 			if (args[1] != 0) dir = args[1];
+			//printf("%s\n", dir);
 			chdir(dir);
 			char *tmp[1] = {""};
 			exec("pwd", tmp);
-			printf("? ");
-			continue;
+
 		} else if (strcmp(cmd, "exit") == 0){
 			printf("exiting\n");
 			free_args(args);
 			return 0;
+
+		} else {
+			if(exec(cmd, args)){
+				printf("invalid cmd\n");
+				return 0;
+			}
 		}
 
-		exec(cmd, args);
 		printf("? ");
 		free_args(args);
 	}
@@ -108,7 +112,9 @@ int loop(){
 int free_args(char **args){
 	int i = 0;
 	for (; i < 512 && (args)[i] != 0; i++){
+		memset(args[i], 0, strlen(args[i]));
 		free((args)[i]);
+		
 	}
 	return(0);
 }
@@ -123,16 +129,22 @@ int parse_cmd(char * cmd, char** args){
 	//*cmd = strtok(buf_cmd, delim);
 	char *tmp_cmd = strtok(buf_cmd, delim);
 	strncpy(cmd, tmp_cmd, 512);
-	args[0] = strncpy((char *) malloc (strlen(tmp_cmd) * sizeof(char)), tmp_cmd, strlen(tmp_cmd));
+	char * arg1 = (char *) malloc (strlen(tmp_cmd) * sizeof(char));
+	memset(arg1, 0, strlen(tmp_cmd) * sizeof(char));
+	args[0] = strncpy(arg1 , tmp_cmd, strlen(tmp_cmd));
 	//printf("replaced cmd is |%s|\n", cmd);
+	
 	int i = 1;
 	char *token = strtok(NULL, delim);
 	while (token != NULL) {
 		//*args[i] = token;
 		//printf("token is |%s|\n", token);
-		int len = strlen(token);
+		int len = strlen(token) + 1;
+		//printf("Token length is %d\n", len);
 		char *cpy = (char *) malloc(len * sizeof(char));
+		memset(cpy, 0, len * sizeof(char));
 		strncpy(cpy, token, len);
+		cpy[len] = '\0';
 		//printf("Copied token is |%s|\n",cpy); 
 		args[i] = cpy;
 		//printf("Placed pointer in array |%p|\n", args[i]);
@@ -150,7 +162,7 @@ int exec(char *cmd, char** args){
 	
 	//if(builtin){
 	if(cid == 0){
-		execvp(cmd, args);
+		if(execvp(cmd, args)) return 1;
 	} else {
 		wait();
 	}
@@ -163,9 +175,9 @@ void handle_sigint(int sig){
 	//printf("\n");
 	if(cid != 0){ 
 		//printf("c: %d, mine: %d\n", cid, getpid());	
-		kill(cid, SIGTERM);	
+		kill(cid, SIGTERM);
+		printf("\n");	
 	}
-	
 }
 
 
